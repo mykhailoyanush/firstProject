1cc18c22a5adc3c6a02dfdf085a60f551cff6e3b
Mykhailo <yanushmisha@gmail.com>
2019-05-27 21:33:39 +0300

    Add Assigment 6

---
+++ budget-app/src/js/main.js
@@ -0,0 +1,22 @@
+let btnStart=document.getElementById('start'),
+    elems = document.querySelectorAll("div[class*='value']"),
+    inputs =document.getElementsByClassName('expenses-item'),
+    approve=document.getElementsByTagName('button')[0],
+    calc=document.getElementsByTagName('button')[2],
+    enters=document.querySelectorAll("input[class='optionalexpenses-item']"),
+    titles=document.querySelector("input[class='choose-income']"),
+    checkBox=document.querySelector("input[id='savings']"),
+    sum=document.querySelector("input[class='choose-sum']"),
+    percent=document.querySelector("input[class='choose-percent']"),
+    year=document.querySelector("input[class='year-value']"),
+    month=document.querySelector("input[class='month-value']"),
+    day=document.querySelector("input[class='day-value']");
+
+//console.log(approve);
+console.log(titles);
+console.log(checkBox);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
